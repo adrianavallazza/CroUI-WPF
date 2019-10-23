b1d7d54925e0618794e7ef02fdd8da04abd0746a
adrianavallazza <adriana.vallazza@gmx.at>
2019-10-23 17:37:40 +0200

    Provided eventhandlers for button controls

--- CroUI/MainWindow.xaml.cs
+++ CroUI/MainWindow.xaml.cs
@@ -25,49 +25,174 @@ namespace CroUI
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        // *** Connect Button
+        private void buttonConnect_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Start Client Object here and try connect to server (Raspi)
         }
+
+        // *** Shape Buttons
 
         private void buttonRectangle_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Robot.moveRectangle();
         }
 
         private void buttonCircle_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Robot.moveCircle();
         }
 
         private void buttonTriangle_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Robot.moveTriangle();
         }
 
         private void buttonStar_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Robot.moveStar();
         }
+
+        // Effect Buttons
 
         private void buttonLed_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Robot.ledEffect();
         }
 
         private void buttonMusic_Click(object sender, RoutedEventArgs e)
         {
-
+            // ToDo
+            // Robot.playMusic();
         }
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
-        {
+        // Arrow Buttons
 
+        private void Window_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up)
+            {
+                textBoxStatus.Text = "Driving forward...";
+                setBtnPressedDesign(buttonArrowUp);
+                // ToDo
+                // Robot.moveForward(sliderSpeed.value);
+            }
+            if (e.Key == Key.Left)
+            {
+                textBoxStatus.Text = "Driving to the left...";
+                setBtnPressedDesign(buttonArrowLeft);
+                // ToDo
+                // Robot.moveLeft(sliderSpeed.value);
+            }
+            if (e.Key == Key.Right)
+            {
+                textBoxStatus.Text = "Driving to the right...";
+                setBtnPressedDesign(buttonArrowRight);              
+                // ToDo
+                // Robot.moveright(sliderSpeed.value);
+            }
+            if (e.Key == Key.Down)
+            {
+                textBoxStatus.Text = "Driving backwards...";
+                setBtnPressedDesign(buttonArrowDown);          
+                // ToDo
+                // Robot.moveBackwards(sliderSpeed.value);
+            }
         }
 
-        private void buttonConnect_Click(object sender, RoutedEventArgs e)
+        private void Window_PreviewKeyUp(object sender, KeyEventArgs e)
         {
+            clearTextBoxStatus();
 
+            if (e.Key == Key.Up)
+            {
+                resetBtnDesign(buttonArrowUp);
+            }
+            if (e.Key == Key.Left)
+            {
+                resetBtnDesign(buttonArrowLeft);
+            }
+            if (e.Key == Key.Right)
+            {
+                resetBtnDesign(buttonArrowRight);
+            }
+            if (e.Key == Key.Down)
+            {
+                resetBtnDesign(buttonArrowDown);
+            }
+        }
+
+        private void buttonArrowUp_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBoxStatus.Text = "Driving forward...";
+            setBtnPressedDesign(buttonArrowUp);
+            // ToDo
+            // Robot.moveForward(sliderSpeed.value);
+        }
+
+        private void buttonArrowLeft_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBoxStatus.Text = "Driving to the left...";
+            setBtnPressedDesign(buttonArrowLeft);
+            // ToDo
+            // Robot.moveLeft(sliderSpeed.value);
+        }
+
+        private void buttonArrowRight_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBoxStatus.Text = "Driving to the right...";
+            setBtnPressedDesign(buttonArrowRight);
+            // ToDo
+            // Robot.moveright(sliderSpeed.value);
+        }
+
+        private void buttonArrowDown_PreviewMouseDown(object sender, MouseButtonEventArgs e)
+        {
+            textBoxStatus.Text = "Driving backwards...";
+            setBtnPressedDesign(buttonArrowDown);
+            // ToDo
+            // Robot.moveBackwards(sliderSpeed.value);
+        }
+
+        private void buttonArrow_PreviewMouseUp(object sender, MouseButtonEventArgs e)
+        {
+            clearTextBoxStatus();
+            resetBtnDesign((Button)sender);
+        }
+
+        private void clearTextBoxStatus()
+        {
+            textBoxStatus.Text = "";
+        }
+
+        private void sliderSpeed_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            // Determine if a button is currently pressed - if yes which one?
+            // Adjust the speed of the movement
+        }
+
+        private void setBtnPressedDesign(Button btn)
+        {
+            var gradientStopCollection = new GradientStopCollection();
+            gradientStopCollection.Add(new GradientStop(Colors.AliceBlue, 0.0));
+            gradientStopCollection.Add(new GradientStop(Colors.CornflowerBlue, 0.5));
+            gradientStopCollection.Add(new GradientStop(Colors.RoyalBlue, 1));
+            btn.Background = new LinearGradientBrush(gradientStopCollection, new Point(0.5, 0), new Point(0.5, 1));
+        }
+
+        private void resetBtnDesign(Button btn)
+        {
+            var gradientStopCollection = new GradientStopCollection();
+            gradientStopCollection.Add(new GradientStop(Colors.WhiteSmoke, 0.0));
+            gradientStopCollection.Add(new GradientStop(Colors.LightGray, 0.25));
+            gradientStopCollection.Add(new GradientStop(Colors.DimGray, 1));
+            btn.Background = new LinearGradientBrush(gradientStopCollection, new Point(0.5, 0), new Point(0.5, 1));
         }
     }
 }
